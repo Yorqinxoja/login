9dbce2d5a6f4b6b36bd9d290d5d14075bd251c0c
Yorqinxoja <yooorknxoja@gmail.com>
2024-10-06 00:20:17 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Layout from './components/Layout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import QuotesPage from './pages/QuotesPage';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <ToastContainer />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/quotes" element={<QuotesPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
